16b20fa0e5420d01ed8ca9b7b98a561396458669
Tron <asfedasov@gmail.com>
2020-09-14 14:17:23 +0300

    A -update 1.0

---
+++ src/com/dragon/arrays/sol01/TwoDimensionalArray.java
@@ -0,0 +1,45 @@
+package sol01;
+
+public class TwoDimensionalArray {
+    public static void toLeft(int[][] array) {
+
+        System.out.println("Пройти с 1-ой до последней строки:");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.printf("%d\t", array[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        System.out.println("Пройти с 1-го до предпоследнего элемента:");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length - 1; j++) {
+                System.out.printf("%d\t", array[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        System.out.println("В текущую ячейку поместить значение следующей и последнему элементу присвоить 0:");
+        int length = array[0].length;
+        for (int j = 0; j < array.length; j++) {
+            System.arraycopy(array[j], 1, array[j], 0, length - 1);
+            array[j][length - 1] = 0;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.printf("%d\t", array[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] array = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}};
+        toLeft(array);
+    }
+}
